7a8e16692fae5c8f7683e0342ca038f0c0b2ea85
Cooper <155586718+notNamn@users.noreply.github.com>
2024-10-20 17:37:04 -0500

    Update login_system.sql

--- login_system.sql
+++ login_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-08-2024 a las 02:37:16
+-- Tiempo de generación: 06-08-2024 a las 21:37:16
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
